24db12570907ec82ee59822a5a5f04f75f4b10fe
Juan Pellicer <jpellicerbarco@gmail.com>
2024-04-26 13:11:02 +0200

    Añadido dropdown y marcado estadísticas como WIP

--- src/app/components/admin/sidebar/contact-us-de-usuario/contact-us-de-usuario.module.ts
+++ src/app/components/admin/sidebar/contact-us-de-usuario/contact-us-de-usuario.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DropdownComponent } from './dropdown/dropdown.component';
 import { ContacUsDeUsuarioRoutingModule } from './contact-us-de-usuario-routing.module';
 
 
 
 @NgModule({
   declarations: [
-    DropdownComponent
   ],
   imports: [
     CommonModule,
