caa34fa120f0764b3e49fddb7b3858811b6e2478
DinoDani30 <124879246+DinoDani30@users.noreply.github.com>
2024-04-30 16:59:42 +0200

    Integración de Notificaciones de user
    Problemas pero con cambios de formato

--- src/app/components/user/sidebar/notificaciones/notificaciones.component.ts
+++ src/app/components/user/sidebar/notificaciones/notificaciones.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
+import { BackendService } from '../../../../services/backend/backend.service';
 
 @Component({
   selector: 'app-notificaciones',
@@ -7,19 +8,49 @@ import { Location } from '@angular/common';
   styleUrl: './notificaciones.component.css'
 })
 export class NotificacionesComponent {
-  notificaciones = [
-    { color: '', titulo: 'Foro al que pertenece', subtitulo: 'TITULO DE NOTIFICACIÓN', cuerpo: 'Cuerpo de la notificación' },
-    { color: '', titulo: 'Foro al que pertenece', subtitulo: 'TITULO DE NOTIFICACIÓN', cuerpo: 'Cuerpo de la notificación' },
-    { color: '', titulo: 'Foro al que pertenece', subtitulo: 'TITULO DE NOTIFICACIÓN', cuerpo: 'Cuerpo de la notificación' },
-    { color: '#727294', titulo: 'Foro al que pertenece', subtitulo: 'TITULO DE NOTIFICACIÓN', cuerpo: 'Cuerpo de la notificación' },
-    { color: '#727294', titulo: 'Foro al que pertenece', subtitulo: 'TITULO DE NOTIFICACIÓN', cuerpo: 'Cuerpo de la notificación' },
-    { color: '#727294', titulo: 'Foro al que pertenece', subtitulo: 'TITULO DE NOTIFICACIÓN', cuerpo: 'Cuerpo de la notificación' },
-    { color: '#727294', titulo: 'Foro al que pertenece', subtitulo: 'TITULO DE NOTIFICACIÓN', cuerpo: 'Cuerpo de la notificación' },
-    { color: '#727294', titulo: 'Foro al que pertenece', subtitulo: 'TITULO DE NOTIFICACIÓN', cuerpo: 'Cuerpo de la notificación' },
-    { color: '#727294', titulo: 'Foro al que pertenece', subtitulo: 'TITULO DE NOTIFICACIÓN', cuerpo: 'Cuerpo de la notificación' }
-  ];
+  // notificaciones = [
+  //   { color: '', titulo: 'Foro al que pertenece', subtitulo: 'TITULO DE NOTIFICACIÓN', cuerpo: 'Cuerpo de la notificación' },
+  // ];
 
-  constructor(private location: Location) { }
+  notificaciones: any[] = [];
+  private respuesta: any[] = [];
+
+  constructor(private location: Location, private backendService: BackendService) { }
+
+  ngOnInit(): void {
+    this.backendService.getNotificacionesGeneral().subscribe(
+      response => {
+        // this.respuesta = response
+        console.log('Notificaciones: ', response);
+      },
+      error => {
+        console.error('Error: ', error);
+      }
+    );
+  }
+
+  formatear(): void {
+    for (const item of this.respuesta) {
+      var data = {
+        color: '',
+        titulo: '',
+        subtitulo: '',
+        cuerpo: ''
+      }
+      if (item.leida) { data.color = '#727294' }
+      data.titulo = item.asunto.idForo // TODO: Cambiar el id del foro por el nombre del foro
+      data.subtitulo = item.titulo
+      data.cuerpo = item.cuerpoNotificacion
+
+      console.log(data); // LOG
+
+      this.notificaciones.push(data);
+    }
+  }
+
+  notificacionLeida(): void {
+
+  }
 
   goBack(): void {
     this.location.back();
